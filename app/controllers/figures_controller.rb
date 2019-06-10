3ad17a1a28181efefe8e0a9f25ab193934590047
Nate Tan <nktan93@gmail.com>
2019-06-10 22:31:44 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,62 @@
 class FiguresController < ApplicationController
-  # add controller methods
+  # Rack Flash
+  require 'rack-flash'
+  enable :sessions
+  use Rack::Flash
+
+  get '/figures' do
+    @figures = Figure.all
+    erb :'/figures/index'
+  end
+
+  get '/figures/new' do
+    #binding.pry
+
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :'/figures/new'
+  end
+
+  post '/figures' do
+    figure = Figure.create(params[:figure])
+    if !params["title"]["name"].empty?
+      figure.titles << Title.create(params[:title])
+      figure.save
+    end
+    if !params["landmark"]["name"].empty?
+      figure.landmarks << Landmark.create(params[:landmark])
+      figure.save
+    end
+    flash[:message] = "Successfully created figure."
+    redirect "figures/#{figure.id}"
+    #binding.pry
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    #binding.pry
+    erb :'/figures/show'
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :'/figures/edit'
+  end
+
+  patch '/figures/:id' do
+    figure = Figure.find(params[:id])
+    figure.update(params[:figure])
+    if !params["title"]["name"].empty?
+      figure.titles << Title.create(params[:title])
+      figure.save
+    end
+    if !params["landmark"]["name"].empty?
+      figure.landmarks << Landmark.create(params[:landmark])
+      figure.save
+    end
+    flash[:message] = "Successfully edited figure."
+    redirect "figures/#{figure.id}"
+  end
 end
