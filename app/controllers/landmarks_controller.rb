4934a3af5a8b9053983f889f2c76d56e076b99bd
Nate Tan <nktan93@gmail.com>
2019-06-11 19:28:04 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -2,4 +2,10 @@ class LandmarksController < ApplicationController
   get '/landmarks/new' do
     erb :'/landmarks/new'
   end
+
+  post '/figures' do
+    landmark = Landmark.create(params[:landmark])
+    flash[:message] = "Successfully created landmark."
+    redirect "landmarks/#{landmark.id}"
+  end
 end
