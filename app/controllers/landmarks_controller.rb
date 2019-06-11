08810e3a0d8ac959611eb31381928b12ef1e4df9
Nate Tan <nktan93@gmail.com>
2019-06-11 20:00:35 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -16,7 +16,7 @@ class LandmarksController < ApplicationController
 
   get '/landmarks/:id' do
     @landmark = Landmark.find(params[:id])
-    binding.pry
+    #binding.pry
     erb :'/landmarks/show'
   end
 
