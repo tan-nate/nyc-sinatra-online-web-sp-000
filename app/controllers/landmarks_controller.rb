d8c43af82904f635143e8eae888d8a3b69a9baeb
Nate Tan <nktan93@gmail.com>
2019-06-11 19:49:10 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,7 +1,7 @@
 class LandmarksController < ApplicationController
   get '/landmarks' do
     @landmarks = Landmark.all
-    erb:'/landmarks/index'
+    erb :'/landmarks/index'
   end
 
   get '/landmarks/new' do
